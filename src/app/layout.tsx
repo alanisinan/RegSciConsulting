5f15c855d5700e3951cb7fa58b61a66c3266fc2b
Sinan Al Ani <sanoman@crimson.ua.edu>
2026-07-03 17:07:04 -0500

    Fix React hydration error caused by browser extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${merriweather.variable}`}>
+      <body className={`${inter.variable} ${merriweather.variable}`} suppressHydrationWarning>
         <Navbar />
         <main style={{ paddingTop: '70px' }}>{children}</main>
         <Footer />
